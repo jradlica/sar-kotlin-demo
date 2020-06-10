93512112a7563138723fca43671f9455ce64f614
Jakub Radlica <jakub.radlica@gmail.com>
2020-06-10 17:19:33 +0200

    Updated demo

--- src/main/kotlin/com/example/specargresolverdemo/SpecArgResolverDemoApplication.kt
+++ src/main/kotlin/com/example/specargresolverdemo/SpecArgResolverDemoApplication.kt
@@ -3,10 +3,14 @@ package com.example.specargresolverdemo
 import net.kaczmarzyk.spring.data.jpa.web.SpecificationArgumentResolver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableJpaRepositories
 class SpecArgResolverDemoApplication : WebMvcConfigurer {
 	fun main(args: Array<String>) {
 		runApplication<SpecArgResolverDemoApplication>(*args)
--- src/test/kotlin/com/example/specargresolverdemo/CustomerControllerTest.kt
+++ src/test/kotlin/com/example/specargresolverdemo/CustomerControllerTest.kt
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -38,6 +39,8 @@ class CustomerControllerTest {
                 .param("id", homer.id.toString())
                 .param("name", "Homer"))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath(".[?(@.name=='Homer')]").exists())
+                .andExpect(jsonPath("$[1]").doesNotExist())
     }
 
     @Test
@@ -46,6 +49,7 @@ class CustomerControllerTest {
                 .param("id", "2L")
                 .param("name", "Homer"))
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").doesNotExist())
     }
 
 }
